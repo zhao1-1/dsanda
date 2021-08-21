ff1adaf97ed03ea82ba11b3df7663cb0968c2d87
有虫快跑 <798998599@qq.com>
2021-08-21 23:15:22 +0800

    (2)

---
+++ datastructure/Test1.java
@@ -0,0 +1,7 @@
+package datastructure;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Solution1 sl1 = new Solution1();
+    }
+}
---
+++ datastructure/Test2.java
@@ -0,0 +1,9 @@
+package datastructure;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Solution2 sl2 = new Solution2();
+
+
+    }
+}
