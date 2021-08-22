e68620a9996a87c6b5ee015aac01ca340f523beb
有虫快跑 <798998599@qq.com>
2021-08-22 15:22:15 +0800

    【2-10】螺旋矩阵

--- datastructure/Solution2.java
+++ datastructure/Solution2.java
@@ -1,6 +1,9 @@
 package datastructure;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution2 {
     
@@ -34,6 +37,47 @@ public class Solution2 {
             System.out.println(Arrays.toString(ints));
         }
         System.out.println("====================");
+    }
+
+    // 解法1：纯按顺序遍历
+    public List<Integer> spiralOrder_1(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (null == matrix || matrix.length == 0) return Collections.emptyList();
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int count = matrix.length * matrix[0].length;
+        while (count > 0) {
+            for (int i = left; i <= right && count > 0; i++) {
+                result.add(matrix[top][i]);
+                count--;
+            }
+            top++;
+            for (int i = top; i <= bottom && count > 0; i++) {
+                result.add(matrix[i][right]);
+                count--;
+            }
+            right--;
+            for (int i = right; i >= left && count > 0; i--) {
+                result.add(matrix[bottom][i]);
+                count--;
+            }
+            bottom--;
+            for (int i = bottom; i >= top && count > 0; i--) {
+                result.add(matrix[i][left]);
+                count--;
+            }
+            left++;
+
+        }
+
+        return result;
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
     }
 
 }
--- datastructure/Test2.java
+++ datastructure/Test2.java
@@ -4,9 +4,12 @@ public class Test2 {
     public static void main(String[] args) {
         Solution2 sl2 = new Solution2();
 
-        sl2.rotate_2(new int[][]{{1}});
-        sl2.rotate_2(new int[][]{{1,2},{3,4}});
-        sl2.rotate_2(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
-        sl2.rotate_2(new int[][]{{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}});
+//        sl2.rotate_2(new int[][]{{1}});
+//        sl2.rotate_2(new int[][]{{1,2},{3,4}});
+//        sl2.rotate_2(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+//        sl2.rotate_2(new int[][]{{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}});
+
+        System.out.println(sl2.spiralOrder_1(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+        System.out.println(sl2.spiralOrder_1(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
     }
 }
