ffac18e2c5c186eacb3eeaf5926f5b4025fbfb44
有虫快跑 <798998599@qq.com>
2021-08-22 13:24:02 +0800

    【2-9】旋转图像

--- datastructure/Solution2.java
+++ datastructure/Solution2.java
@@ -1,4 +1,39 @@
 package datastructure;
 
+import java.util.Arrays;
+
 public class Solution2 {
+    
+    // 解法2：原地旋转
+    public void rotate_2(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+            }
+        }
+    }
+
+    // 解法3：反转代替旋转（先水平翻转，再对角线翻转）
+    public void rotate_3(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-i-1][j];
+                matrix[n-i-1][j] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int[] ints : matrix) {
+            System.out.println(Arrays.toString(ints));
+        }
+        System.out.println("====================");
+    }
+
 }
--- datastructure/Test2.java
+++ datastructure/Test2.java
@@ -4,6 +4,9 @@ public class Test2 {
     public static void main(String[] args) {
         Solution2 sl2 = new Solution2();
 
-
+        sl2.rotate_2(new int[][]{{1}});
+        sl2.rotate_2(new int[][]{{1,2},{3,4}});
+        sl2.rotate_2(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+        sl2.rotate_2(new int[][]{{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}});
     }
 }
