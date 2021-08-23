538c93b252d66f8dad5da6b8777fd8f158813a17
有虫快跑 <798998599@qq.com>
2021-08-23 11:29:10 +0800

    Solution2更新

--- datastructure/Solution2.java
+++ datastructure/Solution2.java
@@ -1,12 +1,272 @@
 package datastructure;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Solution2 {
-    
+
+    public int drinkBeer(int x) {
+        int bottleCount = x;
+        int bodyCount = x;
+        int capCount = x;
+        while (bodyCount >= 3 || capCount >= 7) {
+            while (bodyCount >= 3) {
+                int changeCount = bodyCount / 3;
+                bottleCount += changeCount;
+                bodyCount %= 3;
+                bodyCount += changeCount;
+                capCount += changeCount;
+            }
+            while (capCount >= 7) {
+                int changeCount = capCount / 7;
+                bottleCount += changeCount;
+                capCount %= 7;
+                bodyCount += changeCount;
+                capCount += changeCount;
+            }
+        }
+        return bottleCount;
+    }
+
+
+
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> iSet = new HashSet<>();
+        Set<Integer> jSet = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    iSet.add(i);
+                    jSet.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (iSet.contains(i) || jSet.contains(j)) matrix[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        System.out.println("==============================");
+    }
+
+
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (k == 0) return new int[0];
+        if (shorter == longer) return new int[]{shorter * k};
+
+        int[] a = new int[k+1];
+        for (int i = 0; i <= k; i++) {
+            a[i] = shorter * (k - i) + longer * i;
+        }
+        return a;
+    }
+
+
+    public boolean oneEditAway(String first, String second) {
+        int fN = first.length();
+        int sN = second.length();
+
+        if (Math.abs(fN - sN) > 1) return false;
+        if (first.equals(second)) return true;
+
+        int fixN = 0;
+        int fI = 0;
+        int sI = 0;
+        while (fI < fN && sI < sN) {
+            if (first.charAt(fI++) == second.charAt(sI++)) continue;
+            if (++fixN > 1) return false;
+            if (fN > sN) sI--;
+            if (fN < sN) fI--;
+        }
+        return true;
+    }
+
+
+    public boolean oneEditAway_2(String first, String second) {
+        if (Objects.isNull(first) || Objects.isNull(second)) return false;
+
+        int fN = first.length();
+        int sN = second.length();
+
+        if (Math.abs(fN - sN) > 1) return false;
+        if (fN < sN) return oneEditAway_2(second, first);
+
+        for (int i = 0; i < sN; i++) {
+            if (first.charAt(i) != second.charAt(i))
+                return first.substring(i+1).equals(second.substring(fN == sN ? i+1 : i));
+        }
+        return true;
+    }
+
+
+    public boolean isStraight(int[] nums) {
+        boolean[] dup = new boolean[14];
+        int min = 14;
+        int max = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (dup[nums[i]])
+                    return false;
+                else
+                    dup[nums[i]] = true;
+
+                if (nums[i] < min) min = nums[i];
+                if (nums[i] > max) max = nums[i];
+            }
+        }
+        return (max - min) < nums.length;
+    }
+
+
+    public int[] masterMind(String solution, String guess) {
+        char FLAG = '%';
+        char[] solutions = solution.toCharArray();
+        char[] guesses = guess.toCharArray();
+        int[] result = new int[2];
+        int countA = 0;
+        int countB = 0;
+        for (int i = 0; i < solutions.length; i++) {
+            if (solutions[i] == guesses[i]) {
+                countA++;
+                solutions[i] = guesses[i] = FLAG;
+            }
+        }
+        for (int i = 0; i < solutions.length; i++) {
+            for (int j = 0; j < guesses.length; j++) {
+                if (solutions[i] != FLAG && guesses[j] != FLAG && solutions[i] == guesses[j]) {
+                    countB++;
+                    solutions[i] = guesses[j] = FLAG;
+                }
+            }
+        }
+        result[0] = countA;
+        result[1] = countB;
+        System.out.println(Arrays.toString(result));
+        return result;
+    }
+
+    public int[] masterMind_2(String solution, String guess) {
+        int[] countMap = new int[26];
+        int realCount = 0;
+        int fakeCount = 0;
+        char solC;
+        char gueC;
+        for (int i = 0; i < 4; i++) {
+            solC = solution.charAt(i);
+            gueC = guess.charAt(i);
+            if (solC == gueC){
+                realCount++;
+            } else {
+                if (countMap[solC - 'A'] < 0) fakeCount++;
+                countMap[solC - 'A'] ++;
+
+                if ((countMap[gueC - 'A']) > 0) fakeCount++;
+                countMap[gueC - 'A']--;
+            }
+        }
+        System.out.println(realCount + "," + fakeCount);
+        return new int[]{realCount, fakeCount};
+    }
+
+//    public String tictactoe(String[] board) {
+//        int N = board.length;
+//        int rowCountO = 0;
+//        int colCountO = 0;
+//        int rightCountO = 0;
+//        int leftCountO = 0;
+//        int rowCountX = 0;
+//        int colCountX = 0;
+//        int rightCountX = 0;
+//        int leftCountX = 0;
+//        boolean hasEmpty = false;
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j< N; j++) {
+//                if (!hasEmpty && " " == board[i].charAt(j)) hasEmpty = true;
+//                if ()
+//            }
+//        }
+//    }
+
+
+    // 第一次整个思路可能都是错的！！
+    /*
+    public String tictactoe_2(String[] board) {
+        // 必须加这个判断，因为如果countX=0，0%1==0，1%1==0
+        if (board.length == 1) {
+            if (board[0] == " ")
+                return "Pending";
+            else
+                return board[0];
+        }
+        int N = board.length;
+        int emptyCount = 0;
+        int iO = 0;
+        int jO = 0;
+        int iX = 0;
+        int jX = 0;
+        int countO = 0;
+        int countX = 0;
+        boolean successO = false;
+        boolean successX = false;
+        char currC;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                currC = board[i].charAt(j);
+                if ('O' == currC) {
+                    iO += i;
+                    jO += j;
+                    countO++;
+                }
+                if ('X' == currC) {
+                    iX += i;
+                    jX += j;
+                    countX++;
+                }
+                if (' ' == currC) {
+                    emptyCount++;
+                }
+            }
+        }
+        if (iO % N == 0 && jO % N == 0 && countO % N == 0) successO = true;
+        if (iX % N == 0 && jX % N == 0 && countO % N == 0) successX = true;
+        if (successO && successX) return "Draw";
+        if (!successO && !successX && emptyCount > 0) return "Pending";
+        if (!successO && !successX && emptyCount == 0) return "Draw";
+        if (successO && !successX)
+            return "O";
+        else
+            return "X";
+    }
+    */
+
+
+    // 错误思路，注意是最大跳跃长度，即你可以跳比这个数小的步数。
+    public boolean canJump_x(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (i == nums.length - 1) return true;
+            if (nums[i] == 0) return  false;
+            i += nums[i];
+        }
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int k;
+                for (k = i-1 ; k >= 0; k--) {
+                    if (nums[k] > i-k) break;
+                }
+                if (k < 0 && i != nums.length - 1) return false;
+            }
+        }
+        return true;
+    }
+
     // 解法2：原地旋转
     public void rotate_2(int[][] matrix) {
         int n = matrix.length;
--- datastructure/Test2.java
+++ datastructure/Test2.java
@@ -4,6 +4,59 @@ public class Test2 {
     public static void main(String[] args) {
         Solution2 sl2 = new Solution2();
 
+//        System.out.println(sl2.drinkBeer(10));
+//        System.out.println(sl2.drinkBeer(0));
+//        System.out.println(sl2.drinkBeer(3));
+//        System.out.println(sl2.drinkBeer(7));
+//        System.out.println(sl2.drinkBeer(21));
+
+//        sl2.setZeroes(new int[][]{{0,1,2,0},{3,4,5,2},{1,3,1,5}});
+//        sl2.setZeroes(new int[][]{{1,1,1},{1,0,1},{1,1,1}});
+//        sl2.setZeroes(new int[][]{{0}});
+//        sl2.setZeroes(new int[][]{{1}});
+//        sl2.setZeroes(new int[][]{{0,1}});
+
+//        System.out.println(Arrays.toString(sl2.divingBoard(1,2,3)));
+//        System.out.println(Arrays.toString(sl2.divingBoard(1,2,4)));
+//        System.out.println(Arrays.toString(sl2.divingBoard(5,5,2)));
+//        System.out.println(Arrays.toString(sl2.divingBoard(5,3,0)));
+//        System.out.println(Arrays.toString(sl2.divingBoard(5,5,0)));
+
+//        System.out.println(sl2.oneEditAway("pale", "ple"));
+//        System.out.println(sl2.oneEditAway("pale", "plea"));
+//        System.out.println(sl2.oneEditAway("pale", "plae"));
+//        System.out.println(sl2.oneEditAway("pales", "pal"));
+//        System.out.println(sl2.oneEditAway("teacher", "beacher"));
+
+//        System.out.println(sl2.oneEditAway_2("pale", "ple"));
+//        System.out.println(sl2.oneEditAway_2("pale", "plea"));
+//        System.out.println(sl2.oneEditAway_2("pale", "plae"));
+//        System.out.println(sl2.oneEditAway_2("pales", "pal"));
+//        System.out.println(sl2.oneEditAway_2("teacher", "beacher"));
+
+//        System.out.println(sl2.isStraight(new int[]{0,0,1,2,5}));
+//        System.out.println(sl2.isStraight(new int[]{0,3,4,4,5}));
+//        System.out.println(sl2.isStraight(new int[]{0,8,9,11,12,13}));
+//        System.out.println(sl2.isStraight(new int[]{0,0,9,10,11,13}));
+//        System.out.println(sl2.isStraight(new int[]{0,7,8,11,12,13}));
+
+//        System.out.println(sl2.masterMind("RGBY", "GGRR"));
+//        System.out.println(sl2.masterMind_2("RGBY", "GGRR"));
+//        System.out.println(sl2.masterMind_2("RGRB", "BBBY"));
+
+//        System.out.println(sl2.tictactoe(new String[]{"O X"," XO","X O"}));
+//        System.out.println(sl2.tictactoe(new String[]{"OOX","XXO","OXO"}));
+//        System.out.println(sl2.tictactoe(new String[]{"OOX","XXO","OX "}));
+//        System.out.println(sl2.tictactoe(new String[]{"O"}));
+//        System.out.println(sl2.tictactoe(new String[]{" O    X"," O     ","     O ","XXXXXXX","  O    "," O   O ","O  X OO"}));
+
+//        System.out.println(sl2.canJump(new int[]{2,3,1,1,4}));
+//        System.out.println(sl2.canJump(new int[]{3,2,1,0,4}));
+//        System.out.println(sl2.canJump(new int[]{2,0}));
+//        System.out.println(sl2.canJump(new int[]{2,0,0}));
+//        System.out.println(sl2.canJump(new int[]{8,6,2,0,2,1,1,0,1}));
+//        System.out.println(sl2.canJump(new int[]{7,6,2,0,2,1,1,0,1}));
+
 //        sl2.rotate_2(new int[][]{{1}});
 //        sl2.rotate_2(new int[][]{{1,2},{3,4}});
 //        sl2.rotate_2(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
