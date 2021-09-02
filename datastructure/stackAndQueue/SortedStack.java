26e7ca22fb6ae13eb1e02c49c72d49f104ff780a
有虫快跑 <798998599@qq.com>
2021-09-02 13:57:18 +0800

    顺序栈（最小值总在最上）

--- datastructure/stackAndQueue/SortedStack.java
+++ datastructure/stackAndQueue/SortedStack.java
@@ -4,25 +4,45 @@ package datastructure.stackAndQueue;
  * @author bin2.zhao (D52B48 in ZhangMen)
  * @since 2021/9/1 19:14
  */
+
+import java.util.Stack;
+
+/**
+ * 无论出栈还是进栈，最小的永远得在最上面（栈顶元素永远是最小值）
+ * 本质上，就是弄一个排序栈，从栈顶到底，是由小到大
+ */
 public class SortedStack {
 
-    public SortedStack() {
+    Stack<Integer> stack;
+    Stack<Integer> tempStack;
 
+    public SortedStack() {
+        this.stack = new Stack<>();
+        this.tempStack = new Stack<>();
+        stack.push(-1);
     }
 
     public void push(int val) {
-
+        while (val > stack.peek() && stack.peek() != -1) {
+            tempStack.push(stack.pop());
+        }
+        stack.push(val);
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
     }
 
     public void pop() {
-
+        if (stack.peek() >= 0) {
+            stack.pop();
+        }
     }
 
     public int peek() {
-
+        return stack.peek();
     }
 
     public boolean isEmpty() {
-
+        return stack.peek() < 0;
     }
 }
