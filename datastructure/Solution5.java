3700d3b1baa2bc359a05089462f329b2a1fd08bc
有虫快跑 <798998599@qq.com>
2021-09-07 00:24:08 +0800

    递归的几道题，反转链表啥的

--- datastructure/Solution5.java
+++ datastructure/Solution5.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author bin2.zhao (D52B48 in ZhangMen)
  * @since 2021/9/6 14:20
@@ -44,6 +48,45 @@ public class Solution5 {
         if (i == 2) return 2;
         if (cache2[i] != 0) return cache2[i];
         return ff(i-1) + ff(i-2);
+    }
+
+
+    public int callCellDivision(int hours) {
+        if (hours == 0) return 1;
+        if (hours == 1) return 2;
+        if (hours == 2) return 4;
+        if (hours == 3) return 7;
+        return 2 * callCellDivision(hours - 1) - callCellDivision(hours - 4);
+    }
+
+
+
+    public int[] reversePrint(ListNode head) {
+        List<Integer> result = new ArrayList<>();
+        reverseTravel(head, result);
+        int[] resultArray = new int[result.size()];
+        int i = 0;
+        for (Integer ele : result) {
+            resultArray[i++] = ele;
+        }
+        System.out.println(Arrays.toString(resultArray));
+        return resultArray;
+    }
+    private void reverseTravel(ListNode head, List<Integer> result) {
+        if (head == null) return;
+        reverseTravel(head.next, result);
+        result.add(head.val);
+    }
+
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+//        if (head == null) return null;
+//        if (head.next == null) return head;
+        ListNode reverseNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reverseNode;
     }
 
 }
--- datastructure/Test5.java
+++ datastructure/Test5.java
@@ -17,11 +17,23 @@ public class Test5 {
 //        System.out.println(sl5.fib_2(44));
 
 
-        System.out.println(sl5.climbStairs(2));
-        System.out.println(sl5.climbStairs(3));
-        System.out.println(sl5.climbStairs(5));
-        System.out.println(sl5.climbStairs(7));
-        System.out.println(sl5.climbStairs(44));
+//        System.out.println(sl5.climbStairs(2));
+//        System.out.println(sl5.climbStairs(3));
+//        System.out.println(sl5.climbStairs(5));
+//        System.out.println(sl5.climbStairs(7));
+//        System.out.println(sl5.climbStairs(45));
+
+
+//        System.out.println(sl5.callCellDivision(6));
+//        System.out.println(sl5.callCellDivision(8));
+//        System.out.println(sl5.callCellDivision(2));
+//        System.out.println(sl5.callCellDivision(50));
+
+
+        Solution3 sl3 = new Solution3();
+        sl5.reversePrint(sl3.buildList(new int[]{1,3,2}));
+        sl5.reversePrint(sl3.buildList(new int[]{2}));
+        sl5.reversePrint(sl3.buildList(new int[]{1,2,3,4,5,6,7,8,9,10}));
 
     }
 }
