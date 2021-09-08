c5746cc339ab4736bda825d96dcb8dae9d97b562
有虫快跑 <798998599@qq.com>
2021-09-09 01:03:44 +0800

    插入排序和选择排序

--- datastructure/SortUtil.java
+++ datastructure/SortUtil.java
@@ -6,12 +6,7 @@ package datastructure;
  */
 public class SortUtil {
 
-    private static void swapUtil(int A, int B) {
-        int temp = A;
-        A = B;
-        B = temp;
-    }
-
+    // 数组实现冒泡排序
     public static int[] bubbleSort(int[] a) {
         int n = a.length;
         if (n <= 1) return a;
@@ -32,8 +27,60 @@ public class SortUtil {
     }
 
 
-//    public static ListNode insertionSort(ListNode node) {
-//
-//    }
+    // 数组实现插入排序
+    public static int[] insertionSort(int[] a) {
+
+        return a;
+    }
+
+
+    // 链表实现插入排序
+    public static ListNode insertionSort(ListNode node) {
+        ListNode sortHead = new ListNode();
+        sortHead.next = node;
+        ListNode tail = node;
+        ListNode curr = node.next;
+        while (curr != null) {
+            ListNode former = sortHead;
+            ListNode latter = node;
+            while (latter != curr && latter.val < curr.val) {
+                former = former.next;
+                latter = latter.next;
+            }
+            if (latter != curr) {
+                ListNode temp = curr;
+                tail.next = tail.next.next;
+                temp.next = latter;
+                former.next = temp;
+            } else {
+                tail = tail.next;
+            }
+            curr = curr.next;
+        }
+        return sortHead.next;
+    }
+
+
+
+    // 数组实现选择排序
+    public static int[] selectSort(int[] a) {
+        if (a.length <= 1) return a;
+
+        for (int i = 0; i < a.length - 1; i++) {
+            // 未排序组里找最小值
+            int minIndex = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[minIndex]) minIndex = j;
+            }
+
+            // 交换未排序队首（已排序队尾）元素和最小值
+            if (minIndex != i) {
+                int temp = a[minIndex];
+                a[minIndex] = a[i];
+                a[i] = temp;
+            }
+        }
+        return a;
+    }
 
 }
