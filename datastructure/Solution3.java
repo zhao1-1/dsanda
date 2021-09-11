72cb94eb0dadc38b4204b923cafe65c03d8e12cf
有虫快跑 <798998599@qq.com>
2021-09-12 00:55:45 +0800

    Solution3注释更新完毕

--- datastructure/Solution3.java
+++ datastructure/Solution3.java
@@ -31,6 +31,11 @@ public class Solution3 {
 //        return head;
 //    }
 
+    /**
+     *【3-1】移除链表元素
+     * {LeetCode-203}
+     * 解法1：虚拟头节点
+     */
     public ListNode removeElements(ListNode head, int val) {
         ListNode newHead = new ListNode();
         newHead.next = head;
@@ -42,13 +47,17 @@ public class Solution3 {
                 p = p.next;
             }
         }
-
         this.printList(newHead.next);
-
         return newHead.next;
     }
 
+    /**
+     *【3-1】移除链表元素
+     * {LeetCode-203}
+     * 解法2：【穿针法】（土办法，需要考虑特殊情况）
+     */
     public ListNode removeElements_2(ListNode head, int val) {
+        //（1）考虑：空节点
         if (head == null) return null;
         ListNode p = head;
         while (p.next != null) {
@@ -57,14 +66,19 @@ public class Solution3 {
             else
                 p = p.next;
         }
+        //（2）考虑：头节点
         if (val == head.val) head = head.next;
-
         this.printList(head);
-
         return head;
     }
 
-
+    /**
+     *【3-1】移除链表元素
+     * {LeetCode-203}
+     * 解法3：【辅助链表】（空间复杂度并没有提高，因为并没有新创建空间）
+     *
+     * == 改变链表的万能写法 ==
+     */
     public ListNode removeElements_3(ListNode head, int val) {
         ListNode newHead = new ListNode();
         ListNode newTail = newHead;
@@ -82,8 +96,12 @@ public class Solution3 {
         return newHead.next;
     }
 
-
-    public ListNode removeElements_4(ListNode head, int val) {
+    /**
+     *【3-1】移除链表元素
+     * {LeetCode-203}
+     * 解法3.2：【辅助链表升级（推荐）】，无需临时结点！
+     */
+    public ListNode removeElements_3_2(ListNode head, int val) {
         ListNode newHead = new ListNode();
         ListNode newTail = newHead;
         ListNode curr = head;
@@ -94,48 +112,20 @@ public class Solution3 {
             }
             curr = curr.next;
         }
+        // 亮点在这里，结束处理的时候，一定要把尾节点的next置为null！
         newTail.next = null;
         this.printList(newHead.next);
         return newHead.next;
     }
 
 
-    public ListNode middleNode(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
 
-        this.printList(slow);
-
-        return slow;
-    }
-
-
-    public ListNode middleNode_2(ListNode head) {
-        ListNode p = head;
-        int pLoc = 0;
-        int middleLoc = 0;
-        while (p != null) {
-            p = p.next;
-            pLoc++;
-        }
-        int listLength = pLoc;
-        middleLoc = listLength / 2;
-        p = head;
-        pLoc = 0;
-        while (p != null && pLoc != middleLoc) {
-            p = p.next;
-            pLoc++;
-        }
-        this.printList(p);
-        return p;
-    }
-
-
-
+    /**
+     *【3-2】删除排序链表中的重复元素
+     * {LeetCode-87}
+     * 解法1：【穿针法】
+     * （p.next = p.next.next） ---- 自己想的，像缝针一样
+     */
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) return null;
         ListNode p = head;
@@ -150,6 +140,11 @@ public class Solution3 {
         return head;
     }
 
+    /**
+     *【3-2】删除排序链表中的重复元素
+     * {LeetCode-87}
+     * 解法2：【辅助链表（万能推荐）】
+     */
     public ListNode deleteDuplicates_2(ListNode head) {
         // 初始值给-101，因为题中有每个节点的取值范围，如果范围固定的话，那就只能用我的穿针法了，或者不用虚拟头结点。
         ListNode newHead = new ListNode(-101);
@@ -169,12 +164,20 @@ public class Solution3 {
     }
 
 
-
+    /**
+     *【3-3】合并两个排序的链表
+     * {剑指Offer-25}
+     * 解法1：创建结果链表法即辅助链表法（自己的思路，难得的一把梭）
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode p1 = l1;
-        ListNode p2 = l2;
+
+        //（1）创建虚拟头节点 + tail指针
         ListNode newHead = new ListNode();
         ListNode tail = newHead;
+
+        //（2）确定初始条件、循环结束控制、核心逻辑
+        ListNode p1 = l1;
+        ListNode p2 = l2;
         while (p1 != null && p2 != null) {
             if (p1.val <= p2.val) {
                 tail.next = p1;
@@ -185,44 +188,22 @@ public class Solution3 {
             }
             tail = tail.next;
         }
+
+        //（3）这俩特殊边界条件的处理
         if (p1 == null) tail.next = p2;
         if (p2 == null) tail.next = p1;
+
         this.printList(newHead.next);
         return newHead.next;
     }
 
 
-    // 不能用这个思路，会出现溢出
-//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//        ListNode p1 = l1;
-//        ListNode p2 = l2;
-//        ListNode rHead = new ListNode();
-//        ListNode rTail = rHead;
-//        long num1 = 0;
-//        long num2 = 0;
-//        long sum = 0;
-//        int i1 = 0;
-//        int i2 = 0;
-//        while (p1 != null) {
-//            num1 += p1.val * Math.pow(10, i1++);
-//            p1 = p1.next;
-//        }
-//        while (p2 != null) {
-//            num2 += p2.val * Math.pow(10, i2++);
-//            p2 = p2.next;
-//        }
-//        sum = num1 + num2;
-//        if (sum == 0) return new ListNode(0);
-//        while (sum > 0) {
-//            rTail.next = new ListNode((int) (sum % 10));
-//            sum /= 10;
-//            rTail = rTail.next;
-//        }
-//        this.printList(rHead.next);
-//        return rHead.next;
-//    }
 
-
+    /**
+     *【3-4】两数相加/大数加法*
+     * {LeetCode-2} {Tencent}
+     * 解法1：直接按位相加
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode p1 = l1;
         ListNode p2 = l2;
@@ -258,7 +239,177 @@ public class Solution3 {
     }
 
 
+    /**
+     *【3-4】两数相加/大数加法*
+     * {LeetCode-2} {Tencent}
+     // 不能用这个思路，会出现溢出
+        注意：不能考虑用int、long接收，因为会越界！！
+     */
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode p1 = l1;
+//        ListNode p2 = l2;
+//        ListNode rHead = new ListNode();
+//        ListNode rTail = rHead;
+//        long num1 = 0;
+//        long num2 = 0;
+//        long sum = 0;
+//        int i1 = 0;
+//        int i2 = 0;
+//        while (p1 != null) {
+//            num1 += p1.val * Math.pow(10, i1++);
+//            p1 = p1.next;
+//        }
+//        while (p2 != null) {
+//            num2 += p2.val * Math.pow(10, i2++);
+//            p2 = p2.next;
+//        }
+//        sum = num1 + num2;
+//        if (sum == 0) return new ListNode(0);
+//        while (sum > 0) {
+//            rTail.next = new ListNode((int) (sum % 10));
+//            sum /= 10;
+//            rTail = rTail.next;
+//        }
+//        this.printList(rHead.next);
+//        return rHead.next;
+//    }
 
+
+    /**
+     *【3-5】链表的中间节点
+     * {LeetCode-876}
+     * 解法1：【快慢指针】
+     */
+    /*
+    注意：
+    这种解法和解法2，实际上执行的语句条数是一样的，并非解法1比解法2快一倍！！！
+    并且即使是数组，也并非快两倍！！也就是说根本不存在性能优化！！
+    */
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        this.printList(slow);
+
+        return slow;
+    }
+
+
+    /**
+     *【3-5】链表的中间节点
+     * {LeetCode-876}
+     * 解法2：先遍历找到中间的计数，再输出。（思路简单，实现复杂）
+     */
+    public ListNode middleNode_2(ListNode head) {
+        ListNode p = head;
+        int pLoc = 0;
+        int middleLoc = 0;
+        while (p != null) {
+            p = p.next;
+            pLoc++;
+        }
+        int listLength = pLoc;
+        middleLoc = listLength / 2;
+        p = head;
+        pLoc = 0;
+        while (p != null && pLoc != middleLoc) {
+            p = p.next;
+            pLoc++;
+        }
+        this.printList(p);
+        return p;
+    }
+
+
+    /**
+     *【3-6】链表中倒数第k个节点
+     * {剑指Offer-22} {Baidu}
+     * 解法1-1：【前后指针法（former先走k个）】
+     */
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode former = head;
+        ListNode latter = head;
+        int i = 0;
+        // 考虑fast!=null是为了避免k大于链表长度的问题，当大于长度时，就输出整个链表。
+        while (i < k && latter != null) {
+            latter = latter.next;
+            i++;
+        }
+        while (latter != null) {
+            latter = latter.next;
+            former = former.next;
+        }
+        this.printList(former);
+        return former;
+    }
+
+
+    /**
+     *【3-6】链表中倒数第k个节点
+     * {剑指Offer-22} {Baidu}
+     * 解法1-2：【前后指针法（）】
+     */
+
+    /**
+     *【3-6】链表中倒数第k个节点
+     * {剑指Offer-22} {Baidu}
+     * 解法2：【两次遍历】 ------ 时间复杂度一样的！
+     * "两个指针走一遍和一个指针走两遍，实际上消耗的时间是一样的！"
+     */
+
+
+    /**
+     *【3-7】删除链表倒数第N个结点
+     * {LeetCode-19}
+     * 解法1：【前后双指针 + 特殊情况特殊处理】  --- 自己想出来的
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 这个地方不能返回head，而是需要返回null，因为有一个元素，且k最小是1，所以只有一个元素必被删！
+        if (head == null || head.next == null) return null;
+
+        ListNode former = head;
+        ListNode latter = head;
+        int i = 0;
+        while (i < n && latter != null) {
+            latter = latter.next;
+            i++;
+        }
+
+        // 这一部分的考虑很牛逼，否则12345，删除倒数第五个，很容易仍输出12345
+        if (latter == null) {
+            this.printList(former.next);
+            return former.next;
+        } else {
+            latter = latter.next;
+        }
+
+        while (latter != null) {
+            former = former.next;
+            latter = latter.next;
+        }
+        former.next = former.next.next;
+        this.printList(head);
+        return head;
+    }
+
+
+    /**
+     *【3-7】删除链表倒数第N个结点
+     * {LeetCode-19}
+     * 解法2：【前后双指针 + prev前驱标记节点】
+     */
+
+
+
+    /**
+     *【3-8】反转链表
+     * {LeetCode-206} {Alibaba}
+     * 解法1：【辅助三指针法】（原地反转）---自己想出来的，但是不通用，而且说不定下次再做就想不出来了。
+     */
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) return head;
 
@@ -279,7 +430,11 @@ public class Solution3 {
     }
 
 
-
+    /**
+     *【3-8】反转链表
+     * {LeetCode-206} {Alibaba}
+     * 解法2：【头插法】（原地反转）--- 自己实现，不咋完美，很啰嗦，很容易出bug
+     */
     public ListNode reverseList_2(ListNode head) {
         if (head == null || head.next == null) return head;
         ListNode newHead = new ListNode();
@@ -297,8 +452,11 @@ public class Solution3 {
         return newHead.next;
     }
 
-
-
+    /**
+     *【3-8】反转链表
+     * {LeetCode-206} {Alibaba}
+     * 解法3：【头插法】（原地反转）---标准答案
+     */
     public ListNode reverseList_3(ListNode head) {
         ListNode newHead = null;
         ListNode curr = head;
@@ -313,9 +471,21 @@ public class Solution3 {
     }
 
 
+    /**
+     *【3-9】回文链表*
+     * {LeetCode-234} {蚂蚁金服}
+     * 解法1：辅助数组
+     * 空间复杂度：o(n)
+     * 优点：就是思路清晰
+     */
+    /*
+    （a）把链表转换成数组；
+    （b）利用判断回文数组的算法。
+    */
     public boolean isPalindrome(ListNode head) {
         if (head == null) return false;
 
+        // (a) 统计链表个数，创造数组，以免浪费，当然也可以动态扩容
         ListNode curr = head;
         int n = 0;
         while (curr != null) {
@@ -323,6 +493,8 @@ public class Solution3 {
             n++;
         }
         int[] listNode = new int[n];
+
+        // (b) 把链表的节点值复制给数组
         curr = head;
         int k = 0;
         while (curr != null && k < n) {
@@ -330,6 +502,8 @@ public class Solution3 {
             curr = curr.next;
             k++;
         }
+
+        // (c) 判断回文数组的方法
         int i = 0;
         int j = listNode.length - 1;
         while (i < j) {
@@ -341,6 +515,32 @@ public class Solution3 {
     }
 
 
+
+    /**
+     *【3-9】回文链表*
+     * {LeetCode-234} {蚂蚁金服}
+     * 解法2：利用反转链表
+     * 空间复杂度：o(n)
+     */
+    /*
+    （a）先复制原始链表；
+    （b）反转原始链表；
+    （c）遍历两个链表，比较值是否全部相同；
+    */
+
+
+
+    /**
+     *【3-9】回文链表*
+     * {LeetCode-234} {蚂蚁金服}
+     * 解法3：反转中间往后的链表
+     * 空间复杂度：o(1)（原地判断）
+     */
+    /*
+    （a）利用快慢针找到中间节点；
+    （b）反转中间节点往后的链表；
+    （c）比对前后两部分的值是否全部相同；
+    */
     public boolean isPalindrome_3(ListNode head) {
         if (head == null) return false;
 
@@ -357,7 +557,19 @@ public class Solution3 {
         return true;
     }
 
+    /**
+     *【3-9】回文链表*
+     * {LeetCode-234} {蚂蚁金服}
+     * 解法3-2：反转中间往前的链表
+     * "反转前半部分链表 然后从中间向两边扩散遍历对比"
+     */
 
+
+    /**
+     *【3-9】回文链表*
+     * {LeetCode-234} {蚂蚁金服}
+     * 解法4：利用回文数的判定方法
+     */
     public boolean isPalindrome_4(ListNode head) {
         if (head == null) return false;
 
@@ -375,8 +587,11 @@ public class Solution3 {
     }
 
 
-
-    // 第一次想出来的方法各种p1，2，3的很容易p懵逼
+    /**
+     *【3-10】奇偶链表*
+     * {LeetCode-328} {Tencent}
+     // 第一次想出来的方法各种p1，2，3的很容易p懵逼
+     */
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) return head;
 
@@ -403,14 +618,23 @@ public class Solution3 {
     }
 
 
+    /**
+     *【3-10】奇偶链表*
+     * {LeetCode-328} {Tencent}
+     * 解法2：【穿针法】---自己第二次想出来的
+     */
     public ListNode oddEvenList_2(ListNode head) {
+        // 只需考虑head==null这一种特殊情况即可，head为一个或者两个结点的情况，下面的逻辑都能覆盖。
         if (head == null) return head;
 
         ListNode oddHead = head;
         ListNode oddTail = oddHead;
         ListNode evenHead = head.next;
         ListNode evenTail = evenHead;
+        // 必须得用临时结点！！
         ListNode temp;
+
+        // 边界条件不好找啊！
         while (evenTail != null && evenTail.next != null) {
             temp = oddTail.next.next;
             oddTail.next = oddTail.next.next;
@@ -426,8 +650,22 @@ public class Solution3 {
     }
 
 
-
+    /**
+     *【3-10】奇偶链表*
+     * {LeetCode-328} {Tencent}
+     * 解法3:【两个辅助链表尾插法】
+     * 时间、空间复杂度也没比原地的差，而且思路简单，实现逻辑清晰！
+     * 非常推荐！！
+     */
+    /*
+    这种思路相似的题：
+    + 删除重复元素
+    + 合并两个有序链表
+    + 两数相加
+    + 反转链表
+    */
     public ListNode oddEvenList_3(ListNode head) {
+        // 无需考虑特殊情况！
         ListNode oddHead = new ListNode();
         ListNode oddTail = oddHead;
         ListNode evenHead = new ListNode();
@@ -454,53 +692,12 @@ public class Solution3 {
     }
 
 
-
-
-    public ListNode getKthFromEnd(ListNode head, int k) {
-        ListNode former = head;
-        ListNode latter = head;
-        int i = 0;
-        // 考虑fast!=null是为了避免k大于链表长度的问题，当大于长度时，就输出整个链表。
-        while (i < k && latter != null) {
-            latter = latter.next;
-            i++;
-        }
-        while (latter != null) {
-            latter = latter.next;
-            former = former.next;
-        }
-        this.printList(former);
-        return former;
-    }
-
-
-
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null || head.next == null) return null;
-        ListNode former = head;
-        ListNode latter = head;
-        int i = 0;
-        while (i < n && latter != null) {
-            latter = latter.next;
-            i++;
-        }
-        if (latter == null) {
-            this.printList(former.next);
-            return former.next;
-        } else {
-            latter = latter.next;
-        }
-        while (latter != null) {
-            former = former.next;
-            latter = latter.next;
-        }
-        former.next = former.next.next;
-        this.printList(head);
-        return head;
-    }
-
-
-    // 解法1：【快慢针扣圈法】，不是环乌龟永远在兔子后面，是环的话，俩兽早晚相遇（扣圈）。
+    /**
+     *【3-12】环形链表
+     * {LeetCode-141}
+     // 解法1：【快慢针扣圈法】，不是环乌龟永远在兔子后面，是环的话，俩兽早晚相遇（扣圈）。
+     * 空间复杂度：o(1)
+     */
     public boolean hasCycle_1(ListNode head) {
         // 无需考虑特殊情况
         // if (head == null || head.next == null) return false;
@@ -517,17 +714,27 @@ public class Solution3 {
     }
 
 
-    public boolean hasCycle_1_2(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        while (fast != slow) {
+    /**
+     *【3-12】环形链表
+     * {LeetCode-141}
+     * 解法1-2：【快慢针扣圈法】（仅仅是写法高级一点，本质一样）
+     */
+//    public boolean hasCycle_1_2(ListNode head) {
+//        ListNode fast = head;
+//        ListNode slow = head;
+//        while (fast != slow) {
+//
+//        }
+//        return false;
+//    }
 
-        }
-        return false;
-    }
 
-
-    // 解法2：【哈希表记录】，o(n)内存
+    /**
+     *【3-12】环形链表
+     * {LeetCode-141}
+     // 解法2：【哈希表记录】
+     * 空间复杂度：o(n)
+     */
     public boolean hasCycle_2(ListNode head) {
         Set<ListNode> nodePools = new HashSet<>();
         ListNode curr = head;
@@ -541,8 +748,28 @@ public class Solution3 {
 
 
 
+    /**
+     *【3-13】相交链表
+     * {LeetCode-160}
+     // 解法1：【双指针循环遍历】（官方）
+     // 解析过程见Leecode官方图解！
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
 
-    // 解法2：【哈希表记录】
+
+    /**
+     *【3-13】相交链表
+     * {LeetCode-160}
+     // 解法2：【哈希表记录】
+     */
     public ListNode getIntersectionNode_2(ListNode headA, ListNode headB) {
         Set<ListNode> nodePools = new HashSet<>();
         ListNode currA = headA;
@@ -559,7 +786,12 @@ public class Solution3 {
     }
 
 
-    // 解法3：【循环计数】
+    /**
+     *【3-13】相交链表
+     * {LeetCode-160}
+     // 解法3：【循环计数】
+     * 时间复杂度同官方解法1，都是两个链表需要循环两遍
+     */
     public ListNode getIntersectionNode_3(ListNode headA, ListNode headB) {
         ListNode currA = headA;
         ListNode currB = headB;
@@ -625,6 +857,9 @@ public class Solution3 {
     }
 
 
+    /**
+     *【3-12.2】环形链表的生成
+     */
     public ListNode buildCycleList(int[] inputArray, int pos) {
         if (null == inputArray || inputArray.length == 0) return null;
 
